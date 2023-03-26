b0beb9d7588db4f91ae0630caecb92f9f09c79ed
rumeysa <karakavakrumeysa@gmail.com>
2023-03-26 15:13:37 +0300

    initial commit

---
+++ src/main/java/org/csystem/app/App.java
@@ -0,0 +1,10 @@
+package org.csystem.app;
+
+public class App {
+    public static void main(String[] args)
+    {
+        String s = null;
+
+        s.toLowerCase();
+    }
+}
---
+++ src/main/java/org/csystem/app/app.kt
@@ -0,0 +1,12 @@
+/*----------------------------------------------------------------------------------------------------------------------
+    Kotlin' de gerçek sayı sabitleri üstel olarak yazılabilir. Sayının değeri ne olursa olsun üstel olarak yazılan
+    sabitler Double türdendir
+----------------------------------------------------------------------------------------------------------------------*/
+package org.csystem.app
+
+fun main()
+{
+    val avogadroNumber: Double = 6.02E23
+
+    println("Avogadro Number = $avogadroNumber")
+}
